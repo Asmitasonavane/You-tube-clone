067f0a4fdee222f58b978c1b2b48cb4b7d2e38ca
Asmita Sonavane <ashusonavane98@gmail.com>
2023-05-22 01:11:11 +0530

    Updated css images and links

--- src/App.js
+++ src/App.js
@@ -2,40 +2,40 @@ import Header from './Header';
 import './App.css';
 import Sidebar from './Sidebar';
 import RecommendedVideo from './RecommendedVideo';
-import { BrowserRouter} from "react-router-dom";
-import { Routes, Route,  } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, } from "react-router-dom";
 import SearchPage from './SearchPage';
-function Compo(){
+function Compo() {
   return (
     <div className='app-page'>
-    <Sidebar />
-    <RecommendedVideo />
-  </div>
+      <Sidebar />
+      <RecommendedVideo />
+    </div>
   )
 }
-function Compo1(){
+function Compo1() {
   return (
     <div className='app-page'>
-     <Sidebar />
-     <SearchPage />
-  </div>
+      <Sidebar />
+      <SearchPage />
+    </div>
   )
 }
 function App() {
   return (
     <div className='app'>
-    {/* <h1>Hello , Lets Build Youtube Clone 🚀</h1> */}
-    {/* header  1 component
+      {/* <h1>Hello , Lets Build Youtube Clone </h1> */}
+      {/* header  1 component
     sidebar   2 component
     REcommended Video  3 component */}
-    <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Compo />}> 
-      </Route>
-      <Route path="/search/:searchTerm" element={<Compo1/>} />
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Compo />}>
+          </Route>
+          <Route path="/search/:searchTerm" element={<Compo1 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -5,65 +5,65 @@ import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined';
 import ChannelRow from './ChannelRow';
 function SearchPage() {
   return <div className='searchPage'>
-     <div className="searchFilter">
-        <TuneOutlinedIcon />
-        <h2>FILTER</h2>
-     </div>
-     <hr></hr>
-     <ChannelRow 
-     image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLTtm75C2mjnlajaXXh5hqznGO5M19p79ZgQ&usqp=CAU"
-     Channel="Prachi Madame"
-     verified
-     subs ="190M"
+    <div className="searchFilter">
+      <TuneOutlinedIcon />
+      <h2>FILTER</h2>
+    </div>
+    <hr></hr>
+    <ChannelRow
+      image="https://www.godrej-ananda.net.in/images/other/how-bangalore-got-its-name.webp"
+      Channel="BangaloreDiaries"
+      verified
+      subs="190M"
       noOfVideos={382}
       description="You can find awesome programs here and you can find awesome programming sessions here You can learn new things here!!!"
-/>
-<hr></hr>
-<VideoRow 
-image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLTtm75C2mjnlajaXXh5hqznGO5M19p79ZgQ&usqp=CAU"
-Channel="Prachi Madame"
-subs ="190M"
-title="Awesome Video"
-timestamp= "3 Years Ago"
- noOfVideos={382}
- description="You can find awesome programs here and you can find awesome programming sessions here You can learn new things here!!!"
-/>
-<VideoRow 
-image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLTtm75C2mjnlajaXXh5hqznGO5M19p79ZgQ&usqp=CAU"
-Channel="Prachi Madame"
-subs ="190M"
-title="Awesome Video"
-timestamp= "3 Years Ago"
- noOfVideos={382}
- description="You can find awesome programs here and you can find awesome programming sessions here You can learn new things here!!!"
-/>
-<VideoRow 
-image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLTtm75C2mjnlajaXXh5hqznGO5M19p79ZgQ&usqp=CAU"
-Channel="Prachi Madame"
-subs ="190M"
-title="Awesome Video"
-timestamp= "3 Years Ago"
- noOfVideos={382}
- description="You can find awesome programs here and you can find awesome programming sessions here You can learn new things here!!!"
-/>
-<VideoRow 
-image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLTtm75C2mjnlajaXXh5hqznGO5M19p79ZgQ&usqp=CAU"
-Channel="Prachi Madame"
-subs ="190M"
-title="Awesome Video"
-timestamp= "3 Years Ago"
- noOfVideos={382}
- description="You can find awesome programs here and you can find awesome programming sessions here You can learn new things here!!!"
-/>
-<VideoRow 
-image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLTtm75C2mjnlajaXXh5hqznGO5M19p79ZgQ&usqp=CAU"
-Channel="Prachi Madame"
-subs ="190M"
-title="Awesome Video"
-timestamp= "3 Years Ago"
- noOfVideos={382}
- description="You can find awesome programs here and you can find awesome programming sessions here You can learn new things here!!!"
-/>
+    />
+    <hr></hr>
+    <VideoRow
+      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMtUnaNxxKAS_bDUJeq_DW7bBbUJzs8UhpHg&usqp=CAU"
+      Channel="BangaloreDiaries"
+      subs="190M"
+      title="Sky"
+      timestamp="2 Years Ago"
+      noOfVideos={323}
+      description="You can find awesome programs here and you can find awesome programming sessions here You can learn new things here!!!"
+    />
+    <VideoRow
+      image="https://swall.teahub.io/photos/small/21-218810_beautiful-wallpapers-download-for-mobile-download-beautiful-images.jpg"
+      Channel="BangaloreDiaries"
+      subs="190M"
+      title="Waterfall"
+      timestamp="1 Years Ago"
+      noOfVideos={382}
+      description="You can find awesome programs here and you can find awesome programming sessions here You can learn new things here!!!"
+    />
+    <VideoRow
+      image="https://static.toiimg.com/thumb/msid-31346158,width-748,height-499,resizemode=4,imgsize-114461/.jpg"
+      Channel="BangaloreDiaries"
+      subs="190M"
+      title="Tajmahal"
+      timestamp="3 Years Ago"
+      noOfVideos={383}
+      description="You can find awesome programs here and you can find awesome programming sessions here You can learn new things here!!!"
+    />
+    <VideoRow
+      image="https://www.searchenginejournal.com/wp-content/uploads/2018/10/How-to-Boost-Your-Images%E2%80%99-Visibility-on-Google-Images-760x400.png"
+      Channel="BangaloreDiaries"
+      subs="190M"
+      title="Photography"
+      timestamp="3 Years Ago"
+      noOfVideos={382}
+      description="You can find awesome programs here and you can find awesome programming sessions here You can learn new things here!!!"
+    />
+    <VideoRow
+      image="https://thumbs.dreamstime.com/b/rainbow-love-heart-background-red-wood-60045149.jpg"
+      Channel="BangaloreDiaries"
+      subs="190M"
+      title="Love"
+      timestamp="3 Years Ago"
+      noOfVideos={382}
+      description="You can find awesome programs here and you can find awesome programming sessions here You can learn new things here!!!"
+    />
   </div>;
 }
 
